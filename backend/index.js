c16c52de741c1340398480fbd14dab7d8d336215
Zihad Hossain Nayem <zihadhossainnayem@gmail.com>
2023-09-19 09:24:31 +0600

    route for getting a book from databse by id

--- backend/index.js
+++ backend/index.js
@@ -54,6 +54,21 @@ app.get("/books", async (request, response) => {
   }
 });
 
+/* route for getting a book from database by id */
+app.get("/books/:id", async (request, response) => {
+  try {
+    const { id } = request.params;
+
+    const book = await Book.findById(id);
+    return response.status(200).json({
+      book,
+    });
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: error.message });
+  }
+});
+
 /* mongodb database connect */
 mongoose
   .connect(mongoDBURL)
